9bb1326176ad7d5e82502b903f2e894b0ced3392
sarahmorin <sarahmorin@gwu.edu>
2020-04-27 12:28:38 -0400

    fixes for packaging.
    added long description file for pyup in reST format. fixed typos in
    index. fixed setup configuration and bumpversion config for setup.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     license='MIT',
     description='A Python module for sampling from the Multivariate Hypergeometric distribution.',
     long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md')),
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('long_descript.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
     ),
     author='Sarah Morin',
@@ -49,9 +49,7 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -71,7 +69,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.6',
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
